771e2123e59398d1e621a8167e4a5ee8b8af70b6
Danila Egorov <enterup@gmail.com>
2015-11-03 01:44:59 +0300

    tte

---
+++ RobotControl/RobotControl/Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace RobotControl
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.forwardButton = new System.Windows.Forms.Button();
+            this.leftButton = new System.Windows.Forms.Button();
+            this.rightButton = new System.Windows.Forms.Button();
+            this.backButton = new System.Windows.Forms.Button();
+            this.stopButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.leftCamButton = new System.Windows.Forms.Button();
+            this.rightCamButton = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // forwardButton
+            // 
+            this.forwardButton.Location = new System.Drawing.Point(216, 333);
+            this.forwardButton.Name = "forwardButton";
+            this.forwardButton.Size = new System.Drawing.Size(75, 23);
+            this.forwardButton.TabIndex = 0;
+            this.forwardButton.Text = "Вперед (W)";
+            this.forwardButton.UseVisualStyleBackColor = true;
+            this.forwardButton.Click += new System.EventHandler(this.forwardButton_Click);
+            // 
+            // leftButton
+            // 
+            this.leftButton.Location = new System.Drawing.Point(297, 362);
+            this.leftButton.Name = "leftButton";
+            this.leftButton.Size = new System.Drawing.Size(75, 23);
+            this.leftButton.TabIndex = 1;
+            this.leftButton.Text = "Вправо(D)";
+            this.leftButton.UseVisualStyleBackColor = true;
+            this.leftButton.Click += new System.EventHandler(this.leftButton_Click);
+            // 
+            // rightButton
+            // 
+            this.rightButton.Location = new System.Drawing.Point(135, 362);
+            this.rightButton.Name = "rightButton";
+            this.rightButton.Size = new System.Drawing.Size(75, 23);
+            this.rightButton.TabIndex = 2;
+            this.rightButton.Text = "Влево(A)";
+            this.rightButton.UseVisualStyleBackColor = true;
+            this.rightButton.Click += new System.EventHandler(this.button3_Click);
+            this.rightButton.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.rightButton_KeyPress);
+            // 
+            // backButton
+            // 
+            this.backButton.Location = new System.Drawing.Point(216, 391);
+            this.backButton.Name = "backButton";
+            this.backButton.Size = new System.Drawing.Size(75, 23);
+            this.backButton.TabIndex = 3;
+            this.backButton.Text = "Назад(S)";
+            this.backButton.UseVisualStyleBackColor = true;
+            this.backButton.Click += new System.EventHandler(this.backButton_Click);
+            // 
+            // stopButton
+            // 
+            this.stopButton.Location = new System.Drawing.Point(216, 362);
+            this.stopButton.Name = "stopButton";
+            this.stopButton.Size = new System.Drawing.Size(75, 23);
+            this.stopButton.TabIndex = 4;
+            this.stopButton.Text = "Стоп(F)";
+            this.stopButton.UseVisualStyleBackColor = true;
+            this.stopButton.Click += new System.EventHandler(this.stopButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "IP робота:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(111, 20);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(193, 20);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.Text = "192.168.0.44";
+            // 
+            // leftCamButton
+            // 
+            this.leftCamButton.Location = new System.Drawing.Point(66, 310);
+            this.leftCamButton.Name = "leftCamButton";
+            this.leftCamButton.Size = new System.Drawing.Size(117, 23);
+            this.leftCamButton.TabIndex = 8;
+            this.leftCamButton.Text = "Камера влево(Q)";
+            this.leftCamButton.UseVisualStyleBackColor = true;
+            this.leftCamButton.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // rightCamButton
+            // 
+            this.rightCamButton.Location = new System.Drawing.Point(329, 310);
+            this.rightCamButton.Name = "rightCamButton";
+            this.rightCamButton.Size = new System.Drawing.Size(111, 23);
+            this.rightCamButton.TabIndex = 9;
+            this.rightCamButton.Text = "Камера вправо(E)";
+            this.rightCamButton.UseVisualStyleBackColor = true;
+            this.rightCamButton.Click += new System.EventHandler(this.rightCamButton_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(340, 16);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(114, 23);
+            this.button8.TabIndex = 10;
+            this.button8.Text = "Поехали!";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(40, 46);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(400, 234);
+            this.pictureBox1.TabIndex = 12;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(507, 435);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.rightCamButton);
+            this.Controls.Add(this.leftCamButton);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.stopButton);
+            this.Controls.Add(this.backButton);
+            this.Controls.Add(this.rightButton);
+            this.Controls.Add(this.leftButton);
+            this.Controls.Add(this.forwardButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button forwardButton;
+        private System.Windows.Forms.Button leftButton;
+        private System.Windows.Forms.Button rightButton;
+        private System.Windows.Forms.Button backButton;
+        private System.Windows.Forms.Button stopButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button leftCamButton;
+        private System.Windows.Forms.Button rightCamButton;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ RobotControl/RobotControl/Form1.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+
+namespace RobotControl
+{
+    public partial class Form1 : Form
+    {
+        private TcpClient client;
+        private NetworkStream stream;
+        static int forwardSpeed = 0;
+        static int backwardSpeed = 0;
+        static int rightSpeed = 0;
+        static int leftSpeed = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void forwardButton_Click(object sender, EventArgs e)
+        {
+            String speedString = "";
+            if (forwardSpeed == 0 && backwardSpeed == 0)
+            {
+                speedString = "fw100;\n";
+                forwardSpeed = 100;
+                stream = client.GetStream();
+                Byte[] data = System.Text.Encoding.ASCII.GetBytes("fw100;\n");
+                stream.Write(data, 0, data.Length);
+               
+                return;
+
+            }
+            if(forwardSpeed == 100)
+            {
+                speedString = "fw150;\n";
+                forwardSpeed = 150;
+                stream = client.GetStream();
+                Byte[] data = System.Text.Encoding.ASCII.GetBytes("fw150;\n");
+                stream.Write(data, 0, data.Length);
+               
+                return;
+            }
+            if (forwardSpeed == 150)
+            {
+                speedString = "fw200;\n";
+                forwardSpeed = 200;
+                stream = client.GetStream();
+                Byte[] data = System.Text.Encoding.ASCII.GetBytes("fw200;\n");
+                stream.Write(data, 0, data.Length);
+               
+                return;
+            }
+            if(forwardSpeed == 0 && backwardSpeed == 200)
+            {
+                speedString = "bw150;\n";
+                backwardSpeed = 150;
+                stream = client.GetStream();
+                Byte[] data = System.Text.Encoding.ASCII.GetBytes("bw150;\n");
+                stream.Write(data, 0, data.Length);
+               
+                return;
+            }
+            if(forwardSpeed == 0 && backwardSpeed == 150)
+            {
+                speedString = "bw100;\n";
+                backwardSpeed = 100;
+                stream = client.GetStream();
+                Byte[] data = System.Text.Encoding.ASCII.GetBytes("bw100;\n");
+                stream.Write(data, 0, data.Length);
+               
+                return;
+            }
+            if(forwardSpeed == 0 && backwardSpeed == 100)
+            {
+                speedString = "bw000;\n";
+                backwardSpeed = 0;
+                stream = client.GetStream();
+                Byte[] data = System.Text.Encoding.ASCII.GetBytes("bw000;\n");
+                stream.Write(data, 0, data.Length);
+              
+                return;
+            }
+          
+            
+           
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            client.Close();
+        }
+
+        private void backButton_Click(object sender, EventArgs e)
+        {
+           
+            if(forwardSpeed == 0 && backwardSpeed == 0)
+            {
+            stream = client.GetStream();
+            Byte[] data = System.Text.Encoding.ASCII.GetBytes("bw100;\n");
+            stream.Write(data, 0, data.Length);
+            backwardSpeed = 100;
+            return;
+            }
+            if(forwardSpeed == 0 && backwardSpeed == 100)
+            {
+            stream = client.GetStream();
+            Byte[] data = System.Text.Encoding.ASCII.GetBytes("bw150;\n");
+            stream.Write(data, 0, data.Length);
+            backwardSpeed = 150;
+            return;
+            }
+            if (forwardSpeed == 0 && backwardSpeed == 150)
+            {
+                stream = client.GetStream();
+                Byte[] data = System.Text.Encoding.ASCII.GetBytes("bw200;\n");
+                stream.Write(data, 0, data.Length);
+                backwardSpeed = 200;
+                return;
+            }
+            
+            if (forwardSpeed == 200 && backwardSpeed == 0)
+            {
+                stream = client.GetStream();
+                Byte[] data = System.Text.Encoding.ASCII.GetBytes("fw150;\n");
+                stream.Write(data, 0, data.Length);
+                forwardSpeed = 150;
+                return;
+            }
+
+            if (forwardSpeed == 150 && backwardSpeed == 0)
+            {
+                stream = client.GetStream();
+                Byte[] data = System.Text.Encoding.ASCII.GetBytes("fw100;\n");
+                stream.Write(data, 0, data.Length);
+                forwardSpeed = 100;
+                return;
+            }
+            if (forwardSpeed == 100 && backwardSpeed == 0)
+            {
+                stream = client.GetStream();
+                Byte[] data = System.Text.Encoding.ASCII.GetBytes("fw000;\n");
+                stream.Write(data, 0, data.Length);
+                forwardSpeed = 0;
+                backwardSpeed = 0;
+                return;
+            }
+ 
+        }
+        private void button6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void stopButton_Click(object sender, EventArgs e)
+        {
+            stream = client.GetStream();
+            Byte[] data = System.Text.Encoding.ASCII.GetBytes("bw000;\n");
+            stream.Write(data, 0, data.Length);
+            forwardSpeed = 0;
+            backwardSpeed = 0;
+         
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            String serverName = textBox1.Text;
+            client = new TcpClient(serverName, 2023);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            stream = client.GetStream();
+            Byte[] data = System.Text.Encoding.ASCII.GetBytes("rt100;\n");
+            stream.Write(data, 0, data.Length);
+          
+        }
+
+        private void leftButton_Click(object sender, EventArgs e)
+        {
+            stream = client.GetStream();
+            Byte[] data = System.Text.Encoding.ASCII.GetBytes("lt100;\n");
+            stream.Write(data, 0, data.Length);
+        
+        }
+
+        private void rightButton_KeyPress(object sender, KeyPressEventArgs e)
+        {
+          
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 'W')
+            {
+                forwardButton_Click(sender, e);
+            }
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            
+        }
+
+        private void rightCamButton_Click(object sender, EventArgs e)
+        {
+         //   webBrowser1.Refresh();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+          //  webBrowser1.Refresh();
+            pictureBox1.LoadAsync("http://192.168.0.44:8081/?action=snapshot");
+        }
+    }
+}
